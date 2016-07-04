ff30d7c2da2bfdb0109e9b2bf7221e5d4f134adc
Ash Kumar <kumar.ashwin@outlook.com>
2016-07-04 19:22:33 -0400

    Finished upto the end of 'Accessing and Using Classes' module

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,72 @@
+﻿using Acme.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in invertory
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+        public Product(int productId, string productName, string productDescription) : this()
+        {
+            Id = productId;
+            Name = productName;
+            Description = productDescription;
+        }
+
+        private string _productName;
+        private string _description;
+        private int _productId;
+        private Vendor _vendor;
+
+        public int Id
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string Name
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+        public Vendor Vendor
+        {
+            get
+            {
+                if (_vendor == null)
+                {
+                    _vendor = new Vendor();
+                }
+                return _vendor;
+            }
+            set { _vendor = value; }
+        }
+
+        public string SayHello()
+        {
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from Product");
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Product", this.Name, "sales@abc.com");
+
+            var result = LoggingService.LogAction("saying hello");
+
+            return $"Hello {Name} ({Id}): {Description}";
+        }
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+
+namespace Acme.BizTests
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void SayHelloTest()
+        {
+            var currentProduct = new Product(){
+                Id = 1,
+                Name = "Saw",
+                Description = "15-inch steel blade hand saw",
+            };
+            currentProduct.Vendor.CompanyName = "ABC Corp";
+
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+            var actual = currentProduct.SayHello();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SayHelloParameterizedConstructorTest()
+        {
+            var currentProduct = new Product(1, "Saw", "15-inch steel blade hand saw");
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+            var actual = currentProduct.SayHello();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ProductIsNull()
+        {
+            Product currentProduct = null;
+
+            var actual = currentProduct?.Vendor?.CompanyName;
+
+            Assert.AreEqual(null, actual);
+        }
+    }
+}
